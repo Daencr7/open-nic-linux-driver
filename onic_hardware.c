a4e91a1889050381fd6213b536b21474bc9fc448
edna <edna@ednaPC.tailba9e0b.ts.net>
2026-06-11 01:45:30 +0700

    fix QDMA err

--- onic_hardware.c
+++ onic_hardware.c
@@ -326,6 +326,32 @@ void onic_qdma_init_error_interrupt(unsigned long qdma, u16 vid)
 	       FIELD_SET(QDMA_GLBL_ERR_ARM_MASK, 0));
 	qdma_write_reg(qdev, offset, val);
 
+
+	dev_info(&qdev->pdev->dev,
+	 "QDMA sticky err before clear: glbl=0x%08x dsc=0x%08x trq=0x%08x c2h=0x%08x h2c=0x%08x\n",
+	 qdma_read_reg(qdev, QDMA_OFFSET_GLBL_ERR_STAT),
+	 qdma_read_reg(qdev, QDMA_OFFSET_GLBL_DSC_ERR_STS),
+	 qdma_read_reg(qdev, QDMA_OFFSET_GLBL_TRQ_ERR_STS),
+	 qdma_read_reg(qdev, QDMA_OFFSET_C2H_ERR_STAT),
+	 qdma_read_reg(qdev, QDMA_OFFSET_H2C_ERR_STAT));
+
+	qdma_write_reg(qdev, QDMA_OFFSET_GLBL_TRQ_ERR_STS,
+		       QDMA_GLBL_TRQ_ERR_ALL_MASK);
+	qdma_write_reg(qdev, QDMA_OFFSET_GLBL_DSC_ERR_STS,
+		       QDMA_GLBL_DSC_ERR_ALL_MASK);
+	qdma_write_reg(qdev, QDMA_OFFSET_C2H_ERR_STAT,
+		       QDMA_C2H_ERR_ALL_MASK);
+	qdma_write_reg(qdev, QDMA_OFFSET_H2C_ERR_STAT,
+		       QDMA_H2C_ERR_ALL_MASK);
+			   
+dev_info(&qdev->pdev->dev,
+	 "QDMA sticky err after clear: glbl=0x%08x dsc=0x%08x trq=0x%08x c2h=0x%08x h2c=0x%08x\n",
+	 qdma_read_reg(qdev, QDMA_OFFSET_GLBL_ERR_STAT),
+	 qdma_read_reg(qdev, QDMA_OFFSET_GLBL_DSC_ERR_STS),
+	 qdma_read_reg(qdev, QDMA_OFFSET_GLBL_TRQ_ERR_STS),
+	 qdma_read_reg(qdev, QDMA_OFFSET_C2H_ERR_STAT),
+	 qdma_read_reg(qdev, QDMA_OFFSET_H2C_ERR_STAT));
+
 	for (i = 0; i < NUM_LEAF_ERROR_AGGREGATORS; i++) {
 		u32 err_idx = leaf_error_aggregators[i];
 
